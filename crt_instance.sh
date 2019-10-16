3e5fdc5843533ca5552761cb8e44b5e598d2b2aa
oyj <oyj@local>
2019-10-16 23:13:11 +0900

    some change

--- crt_instance.sh
+++ crt_instance.sh
@@ -2,7 +2,7 @@
 IMAGE_ID="ami-047f7b46bd6dd5d84"
 INSTANCE_TYPE="t2.micro"
 KEY_NAME="test"
-SGGROUPS="sg-61bd2e0b"
+SGGROUPS="sg-19e96c74"
 #SUBNET="subnet-4d383500"
 #aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
 #aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $INSTANCE_TYPE --key-name test --security-group-ids $SGGROUPS --subnet-id $SUBNET
--- stop_instance.sh
+++ stop_instance.sh
@@ -1,2 +1,4 @@
 . ./common.sh
-$A ec2 run-instances stop-
+$A ec2 stop-instances --instance-ids $1
+$A ec2 describe-instance-status --instance-ids $1 --output text | grep -i code
+
